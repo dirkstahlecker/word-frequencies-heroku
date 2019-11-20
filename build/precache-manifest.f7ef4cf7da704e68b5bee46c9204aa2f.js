d028a0941d4595a852907cc2b8f043eab06ac9f8
Dirk Stahlecker <dirkstahlecker@Dirks-MacBook-Pro.local>
2019-11-20 18:15:36 -0500

    remove proxy

---
+++ build/precache-manifest.f7ef4cf7da704e68b5bee46c9204aa2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42599f26b8bc91e19515c7cdc5748ac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0a74f6f4dd7c02a1451",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4842d40087c6cc8aa7b7",
+    "url": "/static/js/2.bee02c83.chunk.js"
+  },
+  {
+    "revision": "d0a74f6f4dd7c02a1451",
+    "url": "/static/js/main.fb2e0845.chunk.js"
+  },
+  {
+    "revision": "afc85d8fb2cf53cece0e",
+    "url": "/static/js/runtime-main.db15bb83.js"
+  }
+]);
